0135fa731b0053a2235ec11f704993c013bfaaba
niketarachhadia <niketa.rachhadia@gmail.com>
2016-12-19 13:09:20 -0600

    Developed from scratch without template

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(document).ready(function(){
+      $('.parallax').parallax();
+    })
+ $('.chips').material_chip();
+});
+
+function moveToAboutMe(){
+	$('html, body').animate({
+        scrollTop: $("#aboutme").offset().top
+    }, 2000);
+}
+function moveToPortfolio(){
+	$('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 2000);
+}
